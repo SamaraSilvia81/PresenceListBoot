2b159684e8648a1ba162e8da79067918122f2346
Samara Silvia <100232025+SamaraSilvia81@users.noreply.github.com>
2023-05-06 13:51:08 -0300

    Add files via upload

---
+++ src/main/java/com/example/presenceList/presenceList/PresenceListApplication.java
@@ -0,0 +1,13 @@
+package com.example.presenceList.presenceList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresenceListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/presenceList/presenceList/PresenceListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.presenceList.presenceList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenceListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
